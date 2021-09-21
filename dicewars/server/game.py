9fb5bf2ad2cada07f62052d08d045243c2024d11
Karel Benes <ibenes@fit.vutbr.cz>
2021-09-21 14:29:19 +0200

    Make passivity-checks configurable

--- dicewars/server/game.py
+++ dicewars/server/game.py
@@ -10,9 +10,6 @@ from .player import Player
 
 from .summary import GameSummary
 
-MAX_PASS_ROUNDS = 8
-MAX_BATTLES_PER_GAME = 10000  # obsevered maximum of 5671 over over 100k games
-
 
 class Game:
     """Instance of the game
@@ -48,6 +45,8 @@ class Game:
         self.nb_battles = 0
 
         self.max_dice_per_area = game_config.getint('MaxDicePerArea')
+        self.max_pass_rounds = game_config.getint('MaximumNoBattleRounds')
+        self.max_battles_per_game = game_config.getint('MaximumBattlesPerGame')
         self.battle_wear_min = game_config.getint('BattleWearMinimum')
 
         deployment_method = game_config['DeploymentMethod']
@@ -370,8 +369,8 @@ class Game:
         bool
             True if a player has won, False otherwise
         """
-        if self.nb_consecutive_end_of_turns // self.nb_players_alive == MAX_PASS_ROUNDS:
-            self.logger.info("Game cancelled because the limit of {} rounds of passing has been reached".format(MAX_PASS_ROUNDS))
+        if self.nb_consecutive_end_of_turns // self.nb_players_alive == self.max_pass_rounds:
+            self.logger.info("Game cancelled because the limit of {} rounds of passing has been reached".format(self.max_pass_rounds))
             for p in self.players.values():
                 if p.get_number_of_areas() > 0:
                     self.eliminate_player(p.get_name())
@@ -379,8 +378,8 @@ class Game:
             self.process_win(None, -1)
             return True
 
-        if self.nb_battles == MAX_BATTLES_PER_GAME:
-            self.logger.info("Game cancelled because the limit of {} battles has been reached".format(MAX_BATTLES_PER_GAME))
+        if self.nb_battles == self.max_battles_per_game:
+            self.logger.info("Game cancelled because the limit of {} battles has been reached".format(self.max_battles_per_game))
             for p in self.players.values():
                 if p.get_number_of_areas() > 0:
                     self.eliminate_player(p.get_name())
