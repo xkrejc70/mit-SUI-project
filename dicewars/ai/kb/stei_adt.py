909976e9256aaf659ba403f32e2444c82a51d607
Karel Benes <ibenes@fit.vutbr.cz>
2021-09-25 09:07:55 +0200

    Polish the stei+transfer bots

--- dicewars/ai/kb/stei_adt.py
+++ dicewars/ai/kb/stei_adt.py
@@ -6,39 +6,24 @@ from dicewars.ai.dt import stei
 
 
 class AI:
-    """Agent using Strength Difference Checking (SDC) strategy
+    """Agent combining STEi with both aggressive and defensive transfers
 
-    This agent prefers moves with highest strength difference
-    and doesn't make moves against areas with higher strength.
+    First, it tries to transfer forces towards borders (blindly, 2/3 of transfers available),
+    then it performs attacks following the dt.stei AI and finally, it pull dice from danger
+    (locally optimal, 1/3 of transfers).
     """
     def __init__(self, player_name, board, players_order, max_transfers):
-        """
-        Parameters
-        ----------
-        game : Game
-
-        Attributes
-        ----------
-            Areas that can make an attack
-        """
         self.player_name = player_name
         self.logger = logging.getLogger('AI')
         self.max_transfers = max_transfers
 
         self.stei = stei.AI(player_name, board, players_order, max_transfers)
-        self.reserved_evacs = 1
+        self.reserved_evacs = max_transfers // 3
+        self.logger.debug(f'STEi_adt will keep {self.reserved_evacs} transfers for evacuations')
 
         self.stage = 'attack'
 
     def ai_turn(self, board, nb_moves_this_turn, nb_transfers_this_turn, nb_turns_this_game, time_left):
-        """AI agent's turn
-
-        Creates a list with all possible moves along with associated strength
-        difference. The list is then sorted in descending order with respect to
-        the SD. A move with the highest SD is then made unless the highest
-        SD is lower than zero - in this case, the agent ends its turn.
-        """
-
         if nb_transfers_this_turn + self.reserved_evacs < self.max_transfers:
             transfer = get_transfer_to_border(board, self.player_name)
             if transfer:
--- dicewars/ai/kb/stei_at.py
+++ dicewars/ai/kb/stei_at.py
@@ -6,21 +6,12 @@ from dicewars.ai.dt import stei
 
 
 class AI:
-    """Agent using Strength Difference Checking (SDC) strategy
+    """Agent combining STEi with aggressive transfers.
 
-    This agent prefers moves with highest strength difference
-    and doesn't make moves against areas with higher strength.
+    First, it tries to transfer forces towards borders, then it proceeds
+    with attacks following the dt.stei AI.
     """
     def __init__(self, player_name, board, players_order, max_transfers):
-        """
-        Parameters
-        ----------
-        game : Game
-
-        Attributes
-        ----------
-            Areas that can make an attack
-        """
         self.player_name = player_name
         self.logger = logging.getLogger('AI')
         self.max_transfers = max_transfers
@@ -31,14 +22,6 @@ class AI:
         self.stage = 'attack'
 
     def ai_turn(self, board, nb_moves_this_turn, nb_transfers_this_turn, nb_turns_this_game, time_left):
-        """AI agent's turn
-
-        Creates a list with all possible moves along with associated strength
-        difference. The list is then sorted in descending order with respect to
-        the SD. A move with the highest SD is then made unless the highest
-        SD is lower than zero - in this case, the agent ends its turn.
-        """
-
         if nb_transfers_this_turn + self.reserved_evacs < self.max_transfers:
             transfer = get_transfer_to_border(board, self.player_name)
             if transfer:
