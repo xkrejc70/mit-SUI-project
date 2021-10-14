a5ff048988ec39fd864da35e15c10fa7558c25dd
Matěj Sojka <xsojka04@stud.fit.vutbr.cz>
2021-10-14 14:55:14 +0200

    mplayer out

--- dicewars/ai/xkuder04/xkuder04.py
+++ dicewars/ai/xkuder04/xkuder04.py
@@ -3,6 +3,8 @@ import random
 import copy
 import numpy as np
 
+from Mplayer import MPlayer
+
 from numpy import inf
 from numpy.lib.function_base import append
 from dicewars.client.game import player
@@ -323,32 +325,6 @@ class AI:
             score = int(round(score*max_score))
             #print(f"Player_name: {player.player_name}; score: {score}")
         #print(f"#################################")
-
-class Mplayer:
-    def __init__(self, board, player_name : int):
-        self.player_name = player_name
-        self.n_dice = board.get_player_dice(self.player_name)
-        self.all_areas = board.get_player_areas(self.player_name)
-        self.border_areas = board.get_player_border(self.player_name)
-        self.inner_areas = [a for a in self.all_areas if a not in self.border_areas]
-        self.n_biggest_region_size = max(len(region) for region in board.get_players_regions(self.player_name))
-        self.n_all_areas = len(self.all_areas)
-        self.n_border_areas = len(self.border_areas)
-        self.n_inner_areas = len(self.inner_areas)
-        self.n_border_dice = sum(a.get_dice() for a in self.border_areas)
-        self.is_alive = bool(self.n_all_areas)
-        #self.print_F()
-
-    def print_F(self):
-        print(f"player_name : {self.player_name}")
-        print(f"n_dice : {self.n_dice}")
-        print(f"all_areas : {[(a.get_name(), a.get_dice()) for a in self.all_areas]}")
-        print(f"border_areas : {[(a.get_name(), a.get_dice()) for a in self.border_areas]}")
-        print(f"inner_areas : {[(a.get_name(), a.get_dice()) for a in self.inner_areas]}")
-        print(f"n_all_areas : {self.n_all_areas}")
-        print(f"n_border_areas : {self.n_border_areas}")
-        print(f"is_alive : {self.is_alive}")
-        print()
         
 # TODO put to new file
 # List of resonable attacks for specified player
